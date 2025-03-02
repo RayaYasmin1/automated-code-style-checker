edc746e95be1ae6bcf3665907ce064a56d1da962
RayaYasmin1 <rayayasmin111@gmail.com>
2025-03-02 19:57:19 +0000

    used autopep8

--- examples/example_bad_variable_name.py
+++ examples/example_bad_variable_name.py
@@ -1,4 +1,6 @@
 # Example file to test custom tool and Flake8 violations
 
-BadVariableName = 5  # This should trigger a violation (variable names should be lowercase)
-AnotherBadVariable = 10  ## This should also trigger a violation (variable names should be lowercase)
+# This should trigger a violation (variable names should be lowercase)
+BadVariableName = 5
+# This should also trigger a violation (variable names should be lowercase)
+AnotherBadVariable = 10
